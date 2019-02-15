dea55b4855b468fc29ec4be991d73afb5ccbe711
Brandon Li <brandon.li820@icloud.com>
2019-02-15 14:17:24 -0700

    update the view for the buttons

--- Js/Views/CollisionTheoryView.js
+++ Js/Views/CollisionTheoryView.js
@@ -15,6 +15,7 @@
 import ScreenView from "../../../Screen/ScreenView.js";
 import Node from "../../../Screen/Node.js";
 import Sim from "../../../SimCore/SimView.js"
+import Slider from "../../../Slider/SliderNode.js";
 // modules
 
 export default class View {
@@ -39,11 +40,28 @@ export default class View {
     return this.sim.addControlPanel( options )
   }
   /**
-   * @param {object} options - the style of the button
+   * @param {object} options - information at Button/TextPushButton.js
    * @return {node} - the button
    */
-  addButtonToControlPanel( options ){
-    return this.sim.addButtonToControlPanel( options );
+  addTextButton( options ){
+    return this.sim.addTextButton( options );
+  }
+  /**
+   * @param {object} options - information at Slider/SliderNode.js
+   * @return {node} - the button
+   */
+  addSlider( options ){
+    let slider = new Slider( options );
+
+    options.parent.addChild( slider.node )
+    return slider;
+  }
+  /**
+   * @param {object} options - information at Buttons/ImageButton.js
+   * @return {node} - the button
+   */
+  addImageButton( options ){
+    return this.sim.addImageButton( options );
   }
 
 
