dcf635518c69d5f6a0bd2f071a3239b93a86b7a5
Brandon Li <brandon.li820@icloud.com>
2019-02-20 14:55:46 -0700

    added Moving api in collision theory

--- Js/Models/CollisionTheoryModel.js
+++ Js/Models/CollisionTheoryModel.js
@@ -13,6 +13,7 @@
 
 // import modules
 import Cart from "./Cart.js";
+import ObservableVariable from "../../../Observe/ObservableVariable.js";
 
 export default class Model {
   /**
@@ -35,8 +36,9 @@ export default class Model {
       direction: "right", // goes in the right direction
       size: this.cartSize,
       mass: 1,
-      velocity: 5,
-      color: "blue"
+      velocity: 25,
+      color: "blue",
+      changeX: 0
     });
 
     // @public {Cart} the second car cart 
@@ -46,13 +48,17 @@ export default class Model {
       direction: "left", // goes in the right direction
       size: this.cartSize,
       mass: 1,
-      velocity: 5,
-      color: "red"
+      velocity: 25,
+      color: "red",
+      changeX: 0
     });
  
     // @public {ObservableVariable} is the simulation playing
-    this.isPlaying = false;
-
+    this.isPlaying = new ObservableVariable( false );
+    // @public {bool} 
+    this.newRun = true;
+    // this tells if it is inside a run or not // this handles functionality
+    // of the pause button.
 
     // @public the mass information
     this.massUnit = "kg";
@@ -62,7 +68,10 @@ export default class Model {
     // @public the velocity information
     this.velocityUnit = "m/s";
     this.velocityLowerBound = 0;
-    this.velocityUpperBound = 10;
+    this.velocityUpperBound = 50;
+
+    // @public the coefficient of restitution
+    this.restitution = 0;
 
   }
   /**
@@ -70,23 +79,82 @@ export default class Model {
    * this is the logic to the sim
    * move the carts when the play is pressed
    */
-  run( mass1Slider, mass2Slider, velocity1Slider, velocity2Slider ){
-    console.log( this.cart1, this.cart2 )
-    // velocity1Slider.setValue( 20 )
+  run(){
+    // alias self
+    var self = this;
 
-  //   var start = null;
-  //   var element = this.cart1;
+    // stats
+    const pixelsPerMeter = 80;
+    const millisecondsPerSecond = 1000;
 
-  //   function step(timestamp) {
-  //     if (!start) start = timestamp;
-  //     var progress = timestamp - start;
-  //     element.style.transform = 'translateX(' + Math.min(progress / 10, 200) + 'px)';
-  //     if (progress < 2000) {
-  //       window.requestAnimationFrame(step);
-  //     }
-  //   }
+    if ( this.newRun ){ // only on a new run, calculate velocities
+      // go from meters per second to pixels per millisecond
+      // m/s * ( 80px / m ) * ( s / 1000 ms ) dimensional analysis
+      this.cart1.changeX = this.cart1.velocity.value * 80 / 1000;
 
-  // window.requestAnimationFrame(step);
+      // since its the other direction, multiple by -1
+      this.cart2.changeX = -1 * this.cart2.velocity.value * 80 / 1000;
+      this.newRun = false; // no longer a new run
+      // this will turn into a new run on the "reset run" button and the 
+      // reset all button
+    }
+
+
+    var ableToCollide = false;
+    if ( self.cart1.x.value < self.cart2.x.value ){
+      // only if the cart1 is less that the second cart
+      // or the first cart is in front of the other cart, we can collid
+      ableToCollide = true;
+    }
+
+    function moveCart( timestamp ) {
+      self.cart1.x.value += self.cart1.changeX;
+      self.cart2.x.value += self.cart2.changeX;
+
+      if ( ableToCollide && self.cart1.x.value >= self.cart2.x.value ){
+        collideCarts();
+      }
+      if ( self.isPlaying.value === true )
+        setTimeout( moveCart , 1 );
+    }
+
+    setTimeout( moveCart, 1 );
+
+    function collideCarts(){
+      self.cart1.changeX *= -1;
+      self.cart2.changeX *= -1;
+    }
+
+    
+  }
+  /**
+   * Restores the initial locations of the cart. This method is called when the 
+   * simulation "Reset Run" button is pressed. 
+   * @public
+   */
+
+  resetRun(){
+    // reset the locations
+    this.cart1.resetLocation();
+    this.cart2.resetLocation();
+    // new run 
+    this.newRun = true;
+    // pause
+    this.isPlaying.value = false;
+  }
+  /**
+   * Restores everything. This method is called when the 
+   * simulation "Reset All" button is pressed. 
+   * @public
+   */
+  resetAll(){
+    // reset the locations
+    this.cart1.reset();
+    this.cart2.reset();
+    // new run 
+    this.newRun = true;
+    // pause
+    this.isPlaying.value = false;
   }
 
 }
