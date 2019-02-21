c7d536574f8c43c60b948456848f89b4e153cac7
Brandon Li <brandon.li820@icloud.com>
2019-02-20 19:59:44 -0700

    close public because of bugs

--- Js/CollisionTheory.js
+++ Js/CollisionTheory.js
@@ -1,30 +1,30 @@
-/**
- * Learning App
- * CollisionTheory.js
- *
- * @author Brandon Li <brandon.li820@icloud.com> 
- * Created on 1/31/19
- * Copyright © 2019 Brandon Li. All rights reserved.
- *
- * Top-level file that creates the sim
- * Main entry point for the sim.
- *
- */
+// /**
+//  * Learning App
+//  * CollisionTheory.js
+//  *
+//  * @author Brandon Li <brandon.li820@icloud.com> 
+//  * Created on 1/31/19
+//  * Copyright © 2019 Brandon Li. All rights reserved.
+//  *
+//  * Top-level file that creates the sim
+//  * Main entry point for the sim.
+//  *
+//  */
 
-"use strict";
+// "use strict";
 
-import Model from "./Models/CollisionTheoryModel.js";
-import View from "./Views/CollisionTheoryView.js";
+// import Model from "./Models/CollisionTheoryModel.js";
+// import View from "./Views/CollisionTheoryView.js";
 
-// create the model
-var collisionTheoryModel = new Model(); 
+// // create the model
+// var collisionTheoryModel = new Model(); 
 
-var sim = {
-  backgroundSrc: "../CollisionTheory/assets/flatiron.png",
-  home: "../sims",
-  title: "Collision Theory",
-  author: "Brandon Li",
-};
+// var sim = {
+//   backgroundSrc: "../CollisionTheory/assets/flatiron.png",
+//   home: "../sims",
+//   title: "Collision Theory",
+//   author: "Brandon Li",
+// };
 
-// create the view
-var CollisionTheoryView = new View( sim, collisionTheoryModel );
+// // create the view
+// var CollisionTheoryView = new View( sim, collisionTheoryModel );
